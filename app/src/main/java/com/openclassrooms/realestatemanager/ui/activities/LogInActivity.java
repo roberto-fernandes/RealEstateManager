42bff0764a979a7b3ee546bc84ca9387ec9042f2
Roberto Fernandes <roberto.fernandes@timewe.com>
2019-03-07 09:53:47 +0000

    Register activity ui

--- app/src/main/java/com/openclassrooms/realestatemanager/ui/activities/LogInActivity.java
+++ app/src/main/java/com/openclassrooms/realestatemanager/ui/activities/LogInActivity.java
@@ -1,15 +1,37 @@
 package com.openclassrooms.realestatemanager.ui.activities;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import com.openclassrooms.realestatemanager.R;
 
 public class LogInActivity extends AppCompatActivity {
 
+    private TextView registerTextView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_second);
+        setContentView(R.layout.activity_log_in);
+
+        setViews();
+        setListeners();
+    }
+
+    private void setViews() {
+        registerTextView = findViewById(R.id.activity_log_in_register_text_view);
+    }
+
+    private void setListeners() {
+        registerTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(LogInActivity.this, RegisterActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/openclassrooms/realestatemanager/ui/activities/RegisterActivity.java
@@ -0,0 +1,16 @@
+package com.openclassrooms.realestatemanager.ui.activities;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.openclassrooms.realestatemanager.R;
+
+public class RegisterActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+}
